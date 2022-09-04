77fba96935f8b9fe81454add0177159b178385e7
unknown <golrale3@gmail.com>
2022-09-04 12:36:59 +0200

    modification models

--- app/models.py
+++ app/models.py
@@ -1,9 +1,7 @@
-from enum import unique
-from tkinter import CASCADE
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from .database import Base
 
@@ -16,7 +14,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('NOW()'))
+                        nullable=False, server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey(
         "users.id", ondelete="CASCADE"), nullable=False)
 
@@ -29,13 +27,12 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('NOW()'))
-    phone_number = Column(String)
+                        nullable=False, server_default=text('now()'))
 
 
 class Vote(Base):
     __tablename__ = "votes"
     user_id = Column(Integer, ForeignKey(
-        "users.id", ondelete=CASCADE), primary_key=True)
+        "users.id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey(
-        "posts.id", ondelete=CASCADE), primary_key=True)
+        "posts.id", ondelete="CASCADE"), primary_key=True)
